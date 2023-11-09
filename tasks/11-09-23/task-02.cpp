de090d6936f612ee8fadefb0126fc35bf182802b
Tim <tim@zolleis.net>
2023-11-09 14:03:41 +0100

    fix(tasks): improve time complexity of fibonacci numbers

--- tasks/11-09-23/task-02.cpp
+++ tasks/11-09-23/task-02.cpp
@@ -7,6 +7,8 @@ bool isEven(int number) {
     return number % 2 == 0;
 }
 
+//TEST: 9780545010221
+//TEST: 9783446464704
 bool isValidISBN(char (&isbn)[13]) {
     int sum = 0;
     for (int i = 0; i <= 13; ++i) {
@@ -16,7 +18,7 @@ bool isValidISBN(char (&isbn)[13]) {
             sum += isbn[i] * 3;
         }
     }
-    //When we're done, we just check if the sum is divisible by 10
+    //When we're done, we just check if the sum is divisible by 10 with no rest
     return sum % 10 == 0;
 }
 
--- tasks/11-09-23/task-03.cpp
+++ tasks/11-09-23/task-03.cpp
@@ -3,6 +3,26 @@
 //
 #include <cstdio>
 
+//This way faster
+double getFastFibonacciNumber(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    }
+
+    double result = 0;
+    double previous = 0;
+    double current = 1;
+    for (int i = 2; i <= n; i++) {
+        result = previous + current;
+        previous = current;
+        current = result;
+    }
+    return result;
+}
+
+//This works but has a high time complexity when n is large
 double getFibonacciNumber(int n) {
     if (n == 0) {
         return 0;
@@ -20,6 +40,7 @@ double getQuotient(int n) {
 int main() {
     for (int i = 1; i <= 20; i++) {
         printf("%i\n", (int) getFibonacciNumber(i));
+        printf("%i\n", (int) getFastFibonacciNumber(i));
         double quotient = getQuotient(i);
         printf("%.15f\n", quotient);
     }
